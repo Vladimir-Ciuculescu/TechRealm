a5d63b821a03026eb2ca2d19c20d4f257efab041
Vladi Ciuculescu <vladimir.ciuculescu@titeica.com>
2021-06-08 11:26:56 +0300

    Added and styled the Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import Header from './components/Header/Header.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import {Container} from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      
+        
+        <Header className = "py-3" />
+        <main>
+          <Container>
+                        <h1>Hello to TechRealm</h1>
+          </Container>
+        </main>
+        <Footer />
+      
+    </>
   );
-}
+} 
 
 export default App;
