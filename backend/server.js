94943d619c465c958556678906fa041ba7216263
Vladi Ciuculescu <vladimir.ciuculescu@titeica.com>
2021-06-10 12:59:43 +0300

    Added env file and changed syntax from require to import

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 //Basic code to make the server run with command : node backend/server
 
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+
+import dotenv from 'dotenv';
+
+import products from './data/products.js';
+
+dotenv.config();
 
 const app = express();
 
@@ -17,6 +22,7 @@ app.get('/api/products', (req, res) => {
     res.json(products);
 })
 
+
 //API to get the specific product
 
 app.get('/api/products/:id', (req, res) => {
@@ -25,4 +31,6 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product);
 })
 
-app.listen(5000, console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
