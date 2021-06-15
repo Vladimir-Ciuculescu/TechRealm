fd5e2f6f0b8e4f5cb3d4559cc7f1d08913a54127
Vladi Ciuculescu <vladimir.ciuculescu@titeica.com>
2021-06-15 10:46:18 +0300

    Getting started with redux and fetched products from redux action

--- frontend/src/screens/HomeScreen/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen/HomeScreen.jsx
@@ -1,37 +1,44 @@
-import React, {useState, useEffect} from 'react';
-import { Row, Col } from 'react-bootstrap';
+import React, {useEffect} from 'react';
+import { Row, Col, Spinner } from 'react-bootstrap';
+import { useDispatch, useSelector} from 'react-redux';
 import Product from '../../components/Product/Product'
-import axios from 'axios';
+import {listProducts} from '../../redux/actions/productActions.js';
 
 const HomeScreen = () => {
 
-    const [products, setProducts] = useState([]);
+    const dispatch = useDispatch();
 
     //Here we fetch the products data from our api
 
+    const productList = useSelector(state => state.productList)
+
+    const { loading, error, products } = productList;
+
     useEffect(() => {
-
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products');
-
-            setProducts(data);
-        }
-
-        fetchProducts();
-    }, [])
+        dispatch(listProducts());
+    }, [dispatch])
+    
 
     return (
         <React.Fragment>
 
-            <h1>Latest news</h1>
+            <h1>Latest Products</h1>
 
-            <Row>
-                {products.map(item => (
-                    <Col key = {item._id} sm = {12} md = {6} lg = {4} xl = {3}>
-                        <Product product = {item} />
-                    </Col>
-                ))}
-            </Row>
+            {loading ? (
+                <Spinner animation="border" role="status">
+                    <span className="sr-only">Loading...</span>
+                </Spinner>
+            ) : error ? (<h3>{error}</h3>) : (
+                     <Row>
+                        {products.map(item => (
+                            <Col key = {item._id} sm = {12} md = {6} lg = {4} xl = {3}>
+                                <Product product = {item} />
+                            </Col>
+                        ))}
+                    </Row>
+            )}
+
+           
         
             
         </React.Fragment>
