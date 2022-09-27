d0e385398fe8623bb4df124d16e749eb765e03cd
Vladi <vladimir.ciuculescu@gmail.com>
2022-09-27 15:21:03 +0300

    Added nodemon and concurrently

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const products = require('./data/dummyData')
 const app = express()
 
 app.get('/', (req, res) => {
-	res.send('API is running !')
+	res.send('API is running !!')
 })
 
 app.get('/products', (req, res) => {
